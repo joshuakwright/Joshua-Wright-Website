ad44ee6ccb3c0e132d125365c1ef461ede45b3c9
Snosh Man <mrjoshuakwright@gmail.com>
2022-06-06 22:03:11 -0700

    Updates

---
+++ precache-manifest.086cb8d918097a213572f6175a77104e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b20d296135131bd6965",
+    "url": "/Joshua-Wright-Website/static/css/main.a42e9c9a.chunk.css"
+  },
+  {
+    "revision": "7b20d296135131bd6965",
+    "url": "/Joshua-Wright-Website/static/js/main.7b20d296.chunk.js"
+  },
+  {
+    "revision": "cfe34446b5b29ad3bdeb",
+    "url": "/Joshua-Wright-Website/static/js/1.cfe34446.chunk.js"
+  },
+  {
+    "revision": "5c94c1331a0cfc60ad31",
+    "url": "/Joshua-Wright-Website/static/js/runtime~main.5c94c133.js"
+  },
+  {
+    "revision": "0f0939b2560af40d00427d1a8378cadd",
+    "url": "/Joshua-Wright-Website/static/media/TempResume.0f0939b2.jpg"
+  },
+  {
+    "revision": "d165006903b36d55887f34677ec122f3",
+    "url": "/Joshua-Wright-Website/static/media/flex.d1650069.jpg"
+  },
+  {
+    "revision": "805e4b2bcffa70a2099ec1fcde6a399e",
+    "url": "/Joshua-Wright-Website/static/media/PilotWorkPhoto.805e4b2b.jpg"
+  },
+  {
+    "revision": "ad9414203c0c1a992e9b3302e46852f1",
+    "url": "/Joshua-Wright-Website/static/media/English102Class.ad941420.jpg"
+  },
+  {
+    "revision": "7bde24300d469fddd9851932d52d723f",
+    "url": "/Joshua-Wright-Website/static/media/ProfilePic.7bde2430.jpg"
+  },
+  {
+    "revision": "f06a27f722997b69eaf5420f85ebf7a7",
+    "url": "/Joshua-Wright-Website/static/media/Mountains.f06a27f7.jpg"
+  },
+  {
+    "revision": "e6ce2b59887243e32de2c5f63379a347",
+    "url": "/Joshua-Wright-Website/index.html"
+  }
+];
