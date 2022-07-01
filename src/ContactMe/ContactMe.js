fb1d3cfbf7298d431d3013bc99eb50a4a5714f72
Snosh Man <mrjoshuakwright@gmail.com>
2022-06-30 23:18:40 -0700

    contact info page done

--- src/ContactMe/ContactMe.js
+++ src/ContactMe/ContactMe.js
@@ -15,11 +15,11 @@ export default function ContactMe() {
                 <dd>-  Phone Number: 615-337-0329</dd>
 
                 <dt className='section-title'>Social Media: </dt>
-                <dd>-  LinkedIn: <a href='linkedin.com/in/joshua-w-307983124'>linkedin.com/in/joshua-w-307983124</a></dd>
+                <dd>-  LinkedIn: <a href='https://linkedin.com/in/joshua-w-307983124'>linkedin.com/in/joshua-w-307983124</a></dd>
                 <dd>-  Instagram: josh_dub</dd>
 
                 <dt className='section-title'>My Code: </dt>
-                <dd>-  Github: github.com/joshuakwright</dd>
+                <dd>-  Github: <a href="https://github.com/joshuakwright">github.com/joshuakwright</a></dd>
             </dl>
         </b>
     </div>
